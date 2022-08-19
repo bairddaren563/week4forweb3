e42815c4160cf620afc69efab50270ae2a9b2cca
“noahjun” <594872049@qq.com>
2022-08-19 22:46:39 +0800

    提交分支8

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "OnGKMT5ef17kEYdlbh92zJBu0HjSIemN"
+    const api_key = "M2JTjEm0xkyhVBfDcCU7PoOIHXtWfaJj"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
@@ -39,7 +39,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "OnGKMT5ef17kEYdlbh92zJBu0HjSIemN"
+      const api_key = "M2JTjEm0xkyhVBfDcCU7PoOIHXtWfaJj"
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
